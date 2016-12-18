b8570e04f3b392bae5a5e92822da71dec2eca40c
大嶋広也 <ooshimahiroya@ooshimahironaris-macbook-air.local>
2016-12-18 15:45:05 +0900

    Initial Commit

---
+++ calcilatorPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calcilatorPro
+//
+//  Created by 大嶋 広也 on 2016/12/18.
+//  Copyright © 2016年 大嶋 広也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
